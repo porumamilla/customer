b08e97c85566f1e88b1d0359f64b4dfe0863e9a4
Raghavendra Porumamilla <raghu@raghavendrasmbp.home>
2019-01-13 15:23:26 -0500

    DAO changes are added

--- src/main/java/springml/customer/CustomerController.java
+++ src/main/java/springml/customer/CustomerController.java
@@ -1,41 +1,52 @@
 package springml.customer;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import springml.customer.dao.CustomerDAO;
 
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
+	@Autowired
+	private CustomerDAO customerDAO;
 	
 	@RequestMapping(value="/{email}", method = RequestMethod.GET)
-	public Customer getCustomer(@PathVariable String email) {
-		Customer customer = new Customer();
-		
-		customer.setEmail("raghu.rao@springml.com");
-		customer.setFirstName("Raghu");
-		customer.setLastName("Porumamilla");
-		customer.setAddress("7 Arbor Cir Cincinnati OH 45255");
+	public Customer getCustomer(@PathVariable String email) throws Exception {
+		Customer customer = customerDAO.getCustomerByEmail(email);
 		return customer;
 	}
 	
-	@RequestMapping(value="/", method = RequestMethod.PUT)
-	public void create(@RequestBody Customer customer) {
+	@RequestMapping(value="/all", method = RequestMethod.GET)
+	public List<Customer> getAllCustomers() throws Exception {
+		List<Customer> customers = customerDAO.getAllCustomers();
+		return customers;
+	}
+	
+	@RequestMapping(value="/create", method = RequestMethod.PUT)
+	public void create(@RequestBody Customer customer) throws Exception {
+		System.out.println("inside create");
+		customerDAO.createCustomer(customer);
 		System.out.println("Following Customer is created");
 		System.out.println(customer);
 	}
 	
-	@RequestMapping(value="/", method = RequestMethod.POST)
-	public void update(@RequestBody Customer customer) {
+	@RequestMapping(value="/update", method = RequestMethod.POST)
+	public void update(@RequestBody Customer customer) throws Exception {
+		customerDAO.updateCustomer(customer);
 		System.out.println("Following Customer is updated");
 		System.out.println(customer);
 	}
 	
-	@RequestMapping(value="/", method = RequestMethod.DELETE)
-	public void delete(@RequestBody Customer customer) {
+	@RequestMapping(value="/delete", method = RequestMethod.DELETE)
+	public void delete(@RequestBody Customer customer) throws Exception {
+		customerDAO.deleteCustomer(customer);
 		System.out.println("Following Customer is deleted");
 		System.out.println(customer);
 	}
---
+++ src/main/java/springml/customer/dao/CustomerDAO.java
@@ -0,0 +1,128 @@
+package springml.customer.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import springml.customer.Customer;
+@Repository
+public class CustomerDAO {
+	private static final String databaseName = "customer_master";
+	private static final String instanceConnectionName = "springmlproject:us-east1:customer";
+	private static final String username = "root";
+	private static final String password = "welcome1";
+
+	private Connection getConnection() throws Exception {
+		String jdbcUrl = String.format(
+				"jdbc:mysql://google/%s?cloudSqlInstance=%s"
+						+ "&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false",
+				databaseName, instanceConnectionName);
+
+		return DriverManager.getConnection(jdbcUrl, username, password);
+	}
+
+	public List<Customer> getAllCustomers() throws Exception {
+		List<Customer> customers = new ArrayList<Customer>();
+		
+		Connection con = null;
+		try {
+			con = getConnection();
+			PreparedStatement ps = con.prepareStatement("select * from profile");
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				Customer customer = new Customer();
+				customer.setEmail(rs.getString("email"));
+				customer.setFirstName(rs.getString("firstName"));
+				customer.setLastName(rs.getString("lastName"));
+				customer.setAddress(rs.getString("address"));
+				customers.add(customer);
+			}
+		} finally {
+			if (con != null) {
+				con.close();
+			}
+		}
+		return customers;
+	}
+	public Customer getCustomerByEmail(String email) throws Exception {
+		Customer customer = new Customer();
+		Connection con = null;
+		try {
+			con = getConnection();
+			PreparedStatement ps = con.prepareStatement("select * from profile where email = ?");
+			ps.setString(1, email);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				customer.setEmail(rs.getString("email"));
+				customer.setFirstName(rs.getString("firstName"));
+				customer.setLastName(rs.getString("lastName"));
+				customer.setAddress(rs.getString("address"));
+			}
+		} finally {
+			if (con != null) {
+				con.close();
+			}
+		}
+		return customer;
+	}
+	
+	public void createCustomer(Customer customer) throws Exception {
+		Connection con = null;
+		try {
+			con = getConnection();
+			PreparedStatement ps = con.prepareStatement("insert into profile(email, firstName, lastName, address) values(?, ?, ?, ?)");
+			ps.setString(1, customer.getEmail());
+			ps.setString(2, customer.getFirstName());
+			ps.setString(3, customer.getLastName());
+			ps.setString(4, customer.getAddress());
+			ps.executeUpdate();
+		} finally {
+			if (con != null) {
+				con.close();
+			}
+		}
+	}
+	
+	public void updateCustomer(Customer customer) throws Exception {
+		Connection con = null;
+		try {
+			con = getConnection();
+			PreparedStatement ps = con.prepareStatement("update profile set firstName =?, lastName = ?, address = ? where email = ?");
+			
+			ps.setString(1, customer.getFirstName());
+			ps.setString(2, customer.getLastName());
+			ps.setString(3, customer.getAddress());
+			
+			ps.setString(4, customer.getEmail());
+			ps.executeUpdate();
+		} finally {
+			if (con != null) {
+				con.close();
+			}
+		}
+	}
+	
+	public void deleteCustomer(Customer customer) throws Exception {
+		Connection con = null;
+		try {
+			con = getConnection();
+			PreparedStatement ps = con.prepareStatement("delete from profile where email = ?");
+			ps.setString(1, customer.getEmail());
+			ps.executeUpdate();
+		} finally {
+			if (con != null) {
+				con.close();
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		CustomerDAO customerDAO = new CustomerDAO();
+		System.out.println(customerDAO.getCustomerByEmail("porumamilla_raghu@yahoo.com"));
+	}
+}
