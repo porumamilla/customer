8313b2bc94c44e6a804a52e51d13e672b198a83c
Raghavendra Porumamilla <raghu@Raghavendras-MacBook-Pro.local>
2019-01-19 11:40:20 -0500

    Change

--- k8s_test.sh
+++ k8s_test.sh
@@ -165,11 +165,11 @@ printf '\n'
 
 for((i=0;i<${#services[@]};i++));do
   #gets the name of the service
-  service_name=$(kubectl get svc -o=jsonpath='{range .items['"$i"']}{.metadata.name}{end}')
+  service_name=$(kubectl get svc -o=jsonpath='{range .items['"$i"']}{.metadata.name}{end}' --namespace=dev)
   #gets the service IP address
-  service_IP=$(kubectl get svc -o=jsonpath='{range .items['"$i"']}{.status.loadBalancer.ingress[0].ip}{end}')
+  service_IP=$(kubectl get svc -o=jsonpath='{range .items['"$i"']}{.status.loadBalancer.ingress[0].ip}{end}' --namespace=dev)
   #gets the service endpoint (IP address and port)
-  service_endpoint=$(kubectl get svc -o=jsonpath='{range .items['"$i"']}{.status.loadBalancer.ingress[0].ip}:{.spec.ports[0].port}{end}')
+  service_endpoint=$(kubectl get svc -o=jsonpath='{range .items['"$i"']}{.status.loadBalancer.ingress[0].ip}:{.spec.ports[0].port}{end} --namespace=dev')
   #returns service name and endpoint
   echo -e service: $service_name "\nendpoint for" $service_name: $service_endpoint
   #checks the service endpoint to see if it is running
